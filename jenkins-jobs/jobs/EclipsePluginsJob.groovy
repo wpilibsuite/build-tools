26a4a9041b56eb0a85f70523611d5f5f5386dd7e
Fredric Silberberg <fred@silberberg.xyz>
2016-09-10 15:04:26 -0700

    Moved eclipse plugin build to parameterized functions for github pull request builder

--- jenkins-jobs/jobs/EclipsePluginsJob.groovy
+++ jenkins-jobs/jobs/EclipsePluginsJob.groovy
@@ -1,16 +1,32 @@
-job('Eclipse Plugins') {
+def releaseFolder = "${System.getProperty('user.home')}/releases/release/eclipse/"
+def releaseJob = job('Eclipse Plugins') {
     scm {
         git('https://github.com/wpilibsuite/EclipsePlugins.git')
     }
     triggers {
         scm('H/15 * * * *')
     }
-    steps {
-        gradle('clean')
-        gradle('build')
-        def releaseFolder = "${System.getProperty('user.home')}/releases/release/eclipse/"
-        shell("rm -rf $releaseFolder")
-        shell("mkdir -p $releaseFolder")
-        shell("cp -r ./edu.wpi.first.wpilib.plugins.updatesite/target/site/* $releaseFolder")
+}
+
+setupBuildSteps(releaseJob)
+setupPublish(releaseJob, releaseFolder)
+
+def setupBuildSteps(job) {
+    job.with {
+        steps {
+            gradle('clean')
+            gradle('build')
+        }
+    }
+}
+
+def setupPublish(job, location) {
+    job.with {
+        publishers {
+            postBuildTask {
+                task('BUILD_SUCCESSFUL', "rm -rf $location && mkdir -p $location && cp -r ./edu.wpi.first.wpilib" +
+                        ".updatesite/target/site/* $location")
+            }
+        }
     }
 }
