51f66c27b0786acf4fd6f851c978dd9dcd745e84
Fredric Silberberg <fred@silberberg.xyz>
2016-10-15 13:31:35 -0700

    Added java-installer builds

---
+++ jenkins-jobs/jobs/JavaInstallerJob.groovy
@@ -0,0 +1,77 @@
+def prJob = job('Java Installer - PR') {
+    scm {
+        git {
+            remote {
+                url('https://github.com/wpilibsuite/java-installer.git')
+                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+            }
+            // This is purposefully not a GString. This is a jenkins environment
+            // variable, not a groovy variable
+            branch('${sha1}')
+        }
+    }
+    triggers {
+        githubPullRequest {
+            admins(['333fred', 'PeterJohnson', 'bradamiller', 'Kevin-OConnor'])
+            orgWhitelist('wpilibsuite')
+            useGitHubHooks()
+        }
+    }
+}
+
+setupProperties(prJob)
+setupBuildSteps(prJob, false)
+
+def developmentJob = job('Java Installer - Development')
+
+setupProperties(developmentJob)
+setupGit(developmentJob)
+setupBuildSteps(developmentJob, true)
+
+def releaseJob = job('Java Installer - Release')
+
+setupProperties(releaseJob)
+setupGit(releaseJob)
+setupBuildSteps(releaseJob, true, ['releaseType=OFFICIAL'])
+
+def setupGit(job) {
+    job.with {
+        scm {
+            git {
+                remote {
+                    url('https://github.com/wpilibsuite/java-installer.git')
+                    branch('*/master')
+                }
+            }
+        }
+        triggers {
+            scm('H/15 * * * *')
+        }
+    }
+}
+
+def setupProperties(job) {
+    job.with {
+        // Note: the pull request builder plugin will fail without this property set.
+        properties {
+            githubProjectUrl('https://github.com/wpilibsuite/java-installer')
+        }
+    }
+}
+
+def setupBuildSteps(job, usePublish, properties = null) {
+    job.with {
+        steps {
+            gradle {
+                tasks('clean')
+                tasks('build')
+                if (usePublish) tasks('publish')
+                if (properties != null) {
+                    properties.each { prop ->
+                        switches("-P$prop")
+                    }
+                }
+            }
+        }
+    }
+}
