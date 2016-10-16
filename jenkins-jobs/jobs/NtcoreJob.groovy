32e4ed151d3b470a472fba4c76f97d252ded7ba0
Fredric Silberberg <fred@silberberg.xyz>
2016-10-15 17:26:03 -0700

    Adds ntcore builds.

---
+++ jenkins-jobs/jobs/NtcoreJob.groovy
@@ -0,0 +1,73 @@
+
+['Windows', 'Mac', 'Linux'].each { platform ->
+    def prJob = job("ntcore $platform - PR") {
+        label(platform.toLowerCase())
+    }
+    setupProperties(prJob)
+    setupPrJob(prJob)
+}
+
+def armPrJob = job('ntcore ARM - PR')
+setupProperties(armPrJob)
+setupPrJob(armPrJob)
+
+def developmentJob = pipelineJob('ntcore - Development') {
+    definition {
+        cps {
+            try {
+                script(readFileFromWorkspace('jenkins-jobs/pipeline-scripts/ntcore-development.groovy'))
+            } catch (Exception e) {
+                script(readFileFromWorkspace('pipeline-scripts/ntcore-development.groovy'))
+            }
+        }
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+}
+
+setupProperties(developmentJob)
+
+def releaseJob = pipelineJob('ntcore - Release') {
+    definition {
+        cps {
+            try {
+                script(readFileFromWorkspace('jenkins-jobs/pipeline-scripts/ntcore-release.groovy'))
+            } catch (Exception e) {
+                script(readFileFromWorkspace('pipeline-scripts/ntcore-release.groovy'))
+            }
+        }
+    }
+}
+
+setupProperties(releaseJob)
+
+def setupProperties(job) {
+    job.with {
+        // Note: the pull request builder plugin will fail without this property set.
+        properties {
+            githubProjectUrl('https://github.com/wpilibsuite/java-installer')
+        }
+    }
+}
+
+def setupPrJob(job) {
+    job.with {
+        scm {
+            git {
+                remote {
+                    url('https://github.com/wpilibsuite/EclipsePlugins.git')
+                    refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+                }
+                branch('${sha1}')
+            }
+        }
+        triggers {
+            githubPullRequest {
+                admins(['333fred', 'PeterJohnson', 'bradamiller', 'Kevin-OConnor'])
+                orgWhitelist('wpilibsuite')
+                useGitHubHooks()
+            }
+        }
+    }
+}
