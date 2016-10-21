abf1a4e573177ded26c6afae1d8bc7e3b176d4a2
Fred Silberberg <fred@silberberg.xyz>
2016-10-20 20:29:18 -0700

    Adds building SmartDashboard. (#10)
    * Adds building SmartDashboard.
    
    * Updates the versions to the official repo.
    
    * Added downstream publishing.

---
+++ jenkins-jobs/jobs/SmartDashboardJob.groovy
@@ -0,0 +1,83 @@
+def basePath = 'SmartDashboard'
+folder(basePath)
+
+def prJob = job("$basePath/SmartDashboard - PR") {
+    scm {
+        git {
+            remote {
+                url('https://github.com/wpilibsuite/SmartDashboard.git')
+                refspec('+refs/pull/*:refs/remotes/origin/pr/*')
+            }
+            // This is purposefully not a GString. This is a jenkins environment
+            // variable, not a groovy variable
+            branch('${sha1}')
+        }
+    }
+    triggers {
+        githubPullRequest {
+            admins(['333fred', 'PeterJohnson', 'bradamiller', 'Kevin-OConnor'])
+            orgWhitelist('wpilibsuite')
+            useGitHubHooks()
+        }
+    }
+}
+
+setupProperties(prJob)
+setupBuildSteps(prJob, false)
+
+def developmentJob = job("$basePath/SmartDashboard - Development") {
+    triggers {
+        scm('H/15 * * * *')
+    }
+    publishers {
+        downstream('Eclipse Plugins/Eclipse Plugins - Development')
+    }
+}
+
+setupGit(developmentJob)
+setupProperties(developmentJob)
+setupBuildSteps(developmentJob, true)
+
+def releaseJob = job("$basePath/SmartDashboard - Release")
+
+setupGit(releaseJob)
+setupProperties(releaseJob)
+setupBuildSteps(releaseJob, true, ['releaseType=OFFICIAL'])
+
+def setupGit(job) {
+    job.with {
+        scm {
+            git {
+                remote {
+                    url('https://github.com/wpilibsuite/SmartDashboard.git')
+                    branch('*/master')
+                }
+            }
+        }
+    }
+}
+
+def setupProperties(job) {
+    job.with {
+        // Note: The pull request builder plugin will fail without this property set.
+        properties {
+            githubProjectUrl('https://github.com/wpilibsuite/SmartDashboard')
+        }
+    }
+}
+
+def setupBuildSteps(job, usePublish, properties = null) {
+    job.with {
+        steps {
+            gradle {
+                tasks('clean')
+                tasks(usePublish ? 'publish' : 'build')
+                if (properties != null) {
+                    properties.each { prop ->
+                        switches("-P$prop")
+                    }
+                }
+            }
+        }
+    }
+}
