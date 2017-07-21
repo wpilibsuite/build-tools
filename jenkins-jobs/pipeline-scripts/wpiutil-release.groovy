1e800fbce64b4d2fbd3e9b65d706aaa6a952242c
Thad House <thadhouse1@gmail.com>
2017-07-21 12:25:12 -0700

    Adds a wpiutil job
    fixes a few things

---
+++ jenkins-jobs/pipeline-scripts/wpiutil-release.groovy
@@ -0,0 +1,53 @@
+stage('build') {
+    def builds = [:]
+    builds['linux'] = {
+        node('linux') {
+            git poll: true, url: 'https://github.com/thadhouse/wpiutil-test.git'
+            sh './gradlew clean build -PjenkinsBuild -PskipAthena -PreleaseBuild -PreleaseType=OFFICIAL'
+            stash includes: 'build/libs/**/*.jar, build/outputs/**/*.*', name: 'linux'
+        }
+    }
+    builds['mac'] = {
+        node('mac') {
+            git poll: true, url: 'https://github.com/thadhouse/wpiutil-test.git'
+            sh './gradlew clean build -PjenkinsBuild -PskipAthena -PreleaseBuild -PreleaseType=OFFICIAL'
+            stash includes: 'build/libs/**/*.jar, build/outputs/**/*.*', name: 'mac'
+        }
+    }
+    builds['windows'] = {
+        node('windows') {
+            git poll: true, url: 'https://github.com/thadhouse/wpiutil-test.git'
+            bat '.\\gradlew.bat  clean build -PjenkinsBuild -PskipAthena -PreleaseBuild -PreleaseType=OFFICIAL'
+            stash includes: 'build/libs/**/*.jar, build/outputs/**/*.*', name: 'windows'
+        }
+    }
+    builds['arm'] = {
+        node {
+            ws("workspace/${env.JOB_NAME}/arm") {
+                git poll: true, url: 'https://github.com/thadhouse/wpiutil-test.git'
+                sh './gradlew clean build -PjenkinsBuild -onlyAthena -PreleaseBuild -PreleaseType=OFFICIAL'
+                stash includes: 'build/libs/**/*.jar, build/outputs/**/*.*', name: 'arm'
+            }
+        }
+    }
+
+    parallel builds
+}
+
+stage('combine') {
+    node {
+        ws("workspace/${env.JOB_NAME}/combine") {
+            git poll: false, url: 'https://github.com/ThadHouse/JenkinsCombiner.git'
+            sh 'git clean -xfd'
+            dir('products') {
+                unstash 'linux'
+                unstash 'mac'
+                unstash 'windows'
+                unstash 'arm'
+            }
+            sh 'chmod +x ./gradlew'
+            sh './gradlew clean publish -Pwpiutil -Prepo=release'
+            archiveArtifacts 'products/*.zip, product/*.jar'
+        }
+    }
+}
