a74bff1db953297eab44ecb70c552a298f2d0e99
Thad House <ThadHouse@users.noreply.github.com>
2017-10-07 11:35:21 -0700

    Adds wpilib publishing (#42)

---
+++ jenkins-jobs/pipeline-scripts/allwpilib-development.groovy
@@ -0,0 +1,53 @@
+stage('build') {
+    def builds = [:]
+    builds['linux'] = {
+        node('linux') {
+            git poll: true, url: 'https://github.com/wpilibsuite/allwpilib.git'
+            sh './gradlew clean build -PjenkinsBuild -PskipAthena -PreleaseBuild -PbuildAll -PbuildHalStaticDeps --console=plain --stacktrace --refresh-dependencies'
+            stash includes: 'build/outputs/**/*.*', name: 'linux'
+        }
+    }
+    builds['mac'] = {
+        node('mac') {
+            git poll: true, url: 'https://github.com/wpilibsuite/allwpilib.git'
+            sh './gradlew clean build -PjenkinsBuild -PskipAthena -PreleaseBuild -PbuildAll -PbuildHalStaticDeps --console=plain --stacktrace --refresh-dependencies'
+            stash includes: 'build/outputs/**/*.*', name: 'mac'
+        }
+    }
+    builds['windows'] = {
+        node('windows') {
+            git poll: true, url: 'https://github.com/wpilibsuite/allwpilib.git'
+            bat '.\\gradlew.bat  clean build -PjenkinsBuild -PskipAthena -PreleaseBuild -PbuildAll -PbuildHalStaticDeps --console=plain --stacktrace --refresh-dependencies'
+            stash includes: 'build/outputs/**/*.*', name: 'windows'
+        }
+    }
+    builds['arm'] = {
+        node {
+            ws("workspace/${env.JOB_NAME}/arm") {
+                git poll: true, url: 'https://github.com/wpilibsuite/allwpilib.git'
+                sh './gradlew clean build -PjenkinsBuild -PonlyAthena -PreleaseBuild -PbuildAll -PbuildHalStaticDeps --console=plain --stacktrace --refresh-dependencies'
+                stash includes: 'build/libs/**/*.jar, build/outputs/**/*.*', name: 'arm'
+            }
+        }
+    }
+
+    parallel builds
+}
+
+stage('combine') {
+    node {
+        ws("workspace/${env.JOB_NAME}/combine") {
+            git poll: false, url: 'https://github.com/wpilibsuite/build-tools.git'
+            sh 'git clean -xfd'
+            dir('combiner/products') {
+                unstash 'linux'
+                unstash 'mac'
+                unstash 'windows'
+                unstash 'arm'
+            }
+            sh 'chmod +x ./combiner/gradlew'
+            sh 'cd ./combiner && ./gradlew publish -Pallwpilib'
+            archiveArtifacts 'combiner/products/**/*.zip, combiner/products/**/*.jar, combiner/outputs/**/*.*'
+        }
+    }
+}
