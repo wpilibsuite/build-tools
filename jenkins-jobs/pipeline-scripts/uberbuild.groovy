727a2f71992a5e5f5c161859caf829b7820a1ec6
Peter Johnson <peter@tortall.net>
2017-11-18 08:52:07 -0800

    Fix Uberbuild for repository changes.
    Still won't work due to package web publishing delays.

--- jenkins-jobs/pipeline-scripts/uberbuild.groovy
+++ jenkins-jobs/pipeline-scripts/uberbuild.groovy
@@ -1,10 +1,13 @@
 node {
-    build job: 'RobotBuilder/RobotBuilder - Release'
-    build job: 'Java Installer/Java Installer - Release'
-    build job: 'ntcore/ntcore - Release'
-    build job: 'SmartDashboard/SmartDashboard - Release'
-    build job: 'OutlineViewer/OutlineViewer - Release'
-    build job: 'CSCore/CSCore - Release'
-    build job: 'WPILib/WPILib - Release', parameters: [string(name: 'docsLocation', value: "${env.HOME}/releases/LOWER_BUILD_TYPE/docs/")], propagate: false
+    build job: 'RobotBuilder/RobotBuilder - Release', propagate: false
+    build job: 'Java Installer/Java Installer - Release', propagate: false
+    build job: 'wpiutil/wpiutil - Release', propagate: false
+    build job: 'ntcore/ntcore - Release', propagate: false
+    build job: 'CSCore/cscore - Release', propagate: false
+    build job: 'SmartDashboard/SmartDashboard - Release', propagate: false
+    build job: 'OutlineViewer/OutlineViewer - Release', propagate: false
+    build job: 'Shuffleboard/Shuffleboard - Release', propagate: false
+    build job: 'WPILib/WPILib - Release', propagate: false
+    build job: 'Documentation/Documentation - BUILD_TYPE', propagate: false
     build job: 'Eclipse Plugins/Eclipse Plugins - BUILD_TYPE'
 }
