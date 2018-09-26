7ddd3178502e08fcdbb4ffc74ba8817456e4e90c
Thad House <thadhouse1@gmail.com>
2018-09-26 10:54:49 -0700

    Add remote publishing job
    Used for things that are not built on jenkins, but need to be uploaded to maven

---
+++ jenkins-jobs/jobs/RemotePublishJob.groovy
@@ -0,0 +1,29 @@
+def basePath = 'remote-publish'
+folder(basePath)
+
+def runJob = job("$basePath/TriggeredRun")
+setupGit(runJob)
+setupBuildSteps(runJob)
+
+def setupGit(job) {
+    job.with {
+        scm {
+            git {
+                remote {
+                    url('https://github.com/ThadHouse/RemotePublish.git')
+                    branch('*/master')
+                }
+            }
+        }
+    }
+}
+
+def setupBuildSteps(job) {
+    job.with {
+        steps {
+            gradle {
+                tasks('build')
+            }
+        }
+    }
+}
